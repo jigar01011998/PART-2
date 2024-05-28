a0981aa1a96704addb9f8a33d946e2f5c6b19982
jigar01011998 <jigar.parekh@edu.turkuamk.fi>
2024-05-28 10:24:20 +0300

    part 2 completed

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Author: Jigar Parekh
 # Part: 2 
 # Name: Rendering a collection, modules
+
+
+
+
+###### Pacakges ############
+
+# npm create vite@latest part1 -- --template react
+# cd part1
+# npm install
+# npx json-server --port 3001 --watch db.json
+# npm install axios
+# npm install json-server --save-dev
+# npm run server
+# https://openweathermap.org/
--- country/src/services/weather.js
+++ country/src/services/weather.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiKey = "API_KEY"; 
+const apiKey = import.meta.env.VITE_OPEN_WEATHER_API_KEY;
 
 const baseUrl = "https://api.openweathermap.org/data/2.5/weather";
 
@@ -10,13 +10,15 @@ const getInfo = (city) => {
     return Promise.reject("OpenWeatherMap API key is missing.");
   }
 
-  console.log("City:", city); 
+  console.log("City:", city);
+  console.log("API Key:", apiKey);
+
   return axios
     .get(`${baseUrl}?q=${city}&appid=${apiKey}&units=metric`)
     .then((res) => res.data)
     .catch((error) => {
       console.error("Error fetching weather data:", error);
-      throw error; 
+      throw error;
     });
 };
 
